ce5fbe37a5a4f376e0b674ce19a70b0f58a8790f
Nathan Walker <earthworn@gmail.com>
2014-09-26 11:43:25 -0500

    A solution.

--- PrimeFactors/PrimeFactors/PrimeFactors.swift
+++ PrimeFactors/PrimeFactors/PrimeFactors.swift
@@ -1,3 +1,13 @@
 func factor(num: Int) -> [Int] {
-  return []
+  return _factor(num, 2)
+}
+
+func _factor(num: Int, divisor: Int) -> [Int] {
+  if num % divisor == 0 {
+    return [divisor] + _factor(num / divisor, divisor)
+  } else if num < divisor {
+    return []
+  } else {
+    return _factor(num, divisor + 1)
+  }
 }
--- PrimeFactors/PrimeFactorsSpec/PrimeFactorsSpec.swift
+++ PrimeFactors/PrimeFactorsSpec/PrimeFactorsSpec.swift
@@ -7,31 +7,31 @@ class PrimeFactorsSpec : SwiftestSuite {
       expect(factor(1)).to(.Equal([]))
     }
     
-    xit("factors 2") {
+    it("factors 2") {
       expect(factor(2)).to(.Equal([2]))
     }
     
-    xit("factors 3") {
+    it("factors 3") {
       expect(factor(3)).to(.Equal([3]))
     }
     
-    xit("factors 4") {
+    it("factors 4") {
       expect(factor(4)).to(.Equal([2, 2]))
     }
     
-    xit("factors 6") {
+    it("factors 6") {
       expect(factor(6)).to(.Equal([2, 3]))
     }
     
-    xit("factors 8") {
+    it("factors 8") {
       expect(factor(8)).to(.Equal([2, 2, 2]))
     }
     
-    xit("factors 9") {
+    it("factors 9") {
       expect(factor(9)).to(.Equal([3, 3]))
     }
     
-    xit("factors 11*13*17*23") {
+    it("factors 11*13*17*23") {
       expect(factor(11 * 13 * 17 * 23)).to(.Equal([11, 13, 17, 23]))
     }
     
